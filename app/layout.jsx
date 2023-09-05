72034f934570a6fc9a8e31f0289cddfa90183f25
ethiris01 <md.ethiris2904@gmail.com>
2023-09-05 19:20:13 +0530

    fix erros

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,26 +6,24 @@ export const metadata = {
   title: "Promptopia",
   description: "Discover & Share Ai Prompts",
 };
-const RootLayout = ({ children }) => {
-  return (
-    <html lang="en">
-      <head>
-        <link rel="icon" type="image/svg+xml" href="/assets/images/logo.svg" />
-      </head>
-      <body>
-        <Provider>
-          <div className="main">
-            <div className="gradient" />
-          </div>
-          <main className="app">
-            <Navbar />
-            {children}
-          </main>
-        </Provider>
-      </body>
-    </html>
-  );
-};
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <head>
+      <link rel="icon" type="image/svg+xml" href="/assets/images/logo.svg" />
+    </head>
+    <body>
+      <Provider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+          <Navbar />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
 export default RootLayout;
 
 // this layout where our root stored like in index.html in react , or main root directory
