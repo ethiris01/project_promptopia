8681d90e6a1327dee8faa13d438f9f5203bad202
ethiris01 <md.ethiris2904@gmail.com>
2023-09-05 19:14:52 +0530

    fix typos

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,39 +1,32 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-// db models
-import User from "@models/user";
 
+import User from "@models/user";
 import { connectToDB } from "@utils/database";
-// for authentication purpose
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-
   callbacks: {
-    // authentication purposes.
     async session({ session }) {
-      const sessionUser = await User.findOne({
-        email: session.user.email,
-      });
-
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
       session.user.id = sessionUser._id.toString();
+
       return session;
     },
-
     async signIn({ account, profile, user, credentials }) {
       try {
-        // serverless => lambda => dynamodb
-        // check if a user already exists
         await connectToDB();
-        const userExists = await User.findOne({
-          email: profile.email,
-        });
 
-        // if not, create new user
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
+
+        // if not, create a new document and save user in MongoDB
         if (!userExists) {
           await User.create({
             email: profile.email,
@@ -41,6 +34,7 @@ const handler = NextAuth({
             image: profile.picture,
           });
         }
+
         return true;
       } catch (error) {
         console.log("Error checking if user exists: ", error.message);
@@ -51,5 +45,3 @@ const handler = NextAuth({
 });
 
 export { handler as GET, handler as POST };
-
-// this file is setup as server.js
