b228abd75d20d03193639af99f0e32621f55a30a
ethiris01 <md.ethiris2904@gmail.com>
2023-09-06 14:57:41 +0530

    fix bug

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -28,7 +28,7 @@ const MyProfile = () => {
   };
   const handleDelete = async (post) => {
     const hasConfirmed = confirm(
-      "Are you sure you want to delete this prompt?"
+      "Are you sure? You want to delete this prompt?"
     );
 
     if (hasConfirmed) {
